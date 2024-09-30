ca52f9a36c7e3b94a0f0be201b257bb0302a4113
ShadyAref <shadyaref10@gmai.com>
2024-09-30 15:20:03 +0300

    feat: Update user score in db everytime game ends

--- src/components/FirstGame.vue
+++ src/components/FirstGame.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
         <button id="closeGameButton" @click="goBack">
-            <svg fill="#ffffff" height="50px" width="50px" version="1.1" id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 476.213 476.213" xml:space="preserve">
+            <svg fill="#000000" height="50px" width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 476.213 476.213" xml:space="preserve">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
                     <polygon
-                        points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107 ">
+                        points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107">
                     </polygon>
                 </g>
             </svg>
         </button>
-        <div id="scoreDisplay">Score: 0</div>
+        <div id="scoreDisplay">0</div>
         <div id="unity-container" class="unity-desktop">
             <canvas id="unity-canvas" width="960" height="600" tabindex="-1"></canvas>
             <div id="unity-loading-bar">
@@ -29,18 +28,57 @@
 
 <script>
 import { initializeUnity } from '@/helpers/UnityLogic';
+import { useProfileStore, useScoreStore } from '@/stores';
+import { mapActions } from 'pinia';
 
 export default {
     name: 'UnityGame',
+
+    data() {
+        return {
+            userData: null,
+            score: 0,
+        };
+    },
+
     mounted() {
         initializeUnity('#unity-canvas', 'firstGame');
+        this.getData();
+
+        setInterval(() => {
+            this.score = Number(document.getElementById('scoreDisplay').innerText);
+        }, 1000);
     },
+
+    computed: {
+        // Computed property to display the formatted score
+        formattedScore() {
+            return this.score
+        }
+    },
+
+    watch: {
+        // Watcher for score changes
+        score(newScore) {
+            if(newScore > 0) {
+                this.setGameWinScore(this.userData.id, 1, this.score)
+            }
+        }
+    },
+
     methods: {
+        ...mapActions(useProfileStore, ['getProfile']),
+        ...mapActions(useScoreStore, ['setGameWinScore']),
+
         goBack() {
             this.$emit('go-back');
+        },
+
+        async getData() {
+            this.userData = await this.getProfile();
         }
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
@@ -49,6 +87,7 @@ button {
     z-index: 99;
     right: 5vw;
 }
+
 #scoreDisplay {
     color: red;
     font-size: 50px;
